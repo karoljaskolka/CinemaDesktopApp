19d8bdb0fb725dc97cf58f2e29d7bd6b3c2dc693
matson19 <241318@student.pwr.edu.pl>
2019-12-03 13:52:43 +0100

    procedures start

--- DesktopApp/App/Environment.cs
+++ DesktopApp/App/Environment.cs
@@ -8,9 +8,9 @@ namespace DesktopApp.App
 {
     static public class Environment
     {
-        static public string DB_SOURCE = "Data Source=LAPTOP-O9JU9206; database=Cinema; Trusted_Connection=yes";
+       // static public string DB_SOURCE = "Data Source=LAPTOP-O9JU9206; database=Cinema; Trusted_Connection=yes";
 
-        // static public string DB_SOURCE = "Data Source=LAPTOP-LVDGN2T2; database=Cinema; Trusted_Connection=yes";
+         static public string DB_SOURCE = "Data Source=LAPTOP-LVDGN2T2; database=Cinema; Trusted_Connection=yes";
 
     }
 }
